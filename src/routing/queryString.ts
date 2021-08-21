6503c7eb96ea61627f03465129632538e368d5d5
Jeff <jeff@jeffcamera.com>
2021-08-21 10:45:54 -0400

    added queryString utils from web repo

---
+++ src/routing/queryString.ts
@@ -0,0 +1,49 @@
+export function parseQueryString(queryString: string) {
+	if (!queryString) {
+		return {} as { [key: string]: string };
+	}
+	if (queryString.startsWith('?')) {
+		queryString = queryString.substring(1);
+	}
+	return queryString
+		.split('&')
+		.reduce((result, param) => {
+			const parts = param.split('=');
+			result[decodeURIComponent(parts[0])] = decodeURIComponent(parts[1]);
+			return result;
+		}, {} as { [key: string]: string })
+}
+export function createQueryString(kvps: { [key: string]: string | Array<string> }) {
+	if (kvps) {
+		const qsKvps = Object
+			.keys(kvps)
+			.reduce<string[]>(
+				(qsKvps, key) => {
+					const
+						qsKey = encodeURIComponent(key),
+						value = kvps[key];
+					if (value == null) {
+						qsKvps.push(qsKey);
+					} else if (
+						typeof value === 'string' ||
+						typeof value === 'number' ||
+						typeof value === 'boolean'
+					) {
+						qsKvps.push(qsKey + '=' + encodeURIComponent(value));
+					} else if (Array.isArray(value)) {
+						value.forEach(
+							item => {
+								qsKvps.push(qsKey + '=' + encodeURIComponent(item));
+							}
+						);
+					}
+					return qsKvps;
+				},
+				[]
+			);
+		if (qsKvps.length) {
+			return '?' + qsKvps.join('&');
+		}
+	}
+	return '';
+}
